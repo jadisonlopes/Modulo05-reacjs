295229091db99b0b8f2e0db90300156107143d8d
Jadison Lopes <jadison.lopes.s@gmail.com>
2019-07-25 11:42:34 -0300

    feature: Listing repositories of the github

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 
-import { Title } from './styles';
+import api from '../../services/api';
+import { Container, Form, SubmitButton, List } from './styles';
 
-export default function Main() {
-  return <Title>Hello World</Title>;
+export default class Main extends Component {
+  state = {
+    newRepository: '',
+    repositories: [],
+    loading: false,
+  };
+
+  handleInputChange = env => {
+    this.setState({ newRepository: env.target.value });
+  };
+
+  handleSubmit = async env => {
+    env.preventDefault();
+
+    this.setState({ loading: true });
+
+    const { newRepository, repositories } = this.state;
+
+    const response = await api.get(`/repos/${newRepository}`);
+
+    const data = {
+      name: response.data.full_name,
+    };
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepository: '',
+      loading: false,
+    });
+  };
+
+  render() {
+    const { newRepository, loading, repositories } = this.state;
+
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositorios
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adicionar repositório"
+            value={newRepository}
+            onChange={this.handleInputChange}
+          />
+
+          <SubmitButton loading={loading}>
+            {loading ? (
+              <FaSpinner color="#FFF" size={14} />
+            ) : (
+              <FaPlus color="#FFF" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+        <List>
+          {repositories.map(repository => (
+            <li key={repository.name}>
+              <small>{repository.name}</small>
+              <a href="#">Detalhe</a>
+            </li>
+          ))}
+        </List>
+      </Container>
+    );
+  }
 }
